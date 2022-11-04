5adc4508a17f49f3a8ee53db60a31588aaf95a8c
pc <rahimjangi1982@gmail.com>
2022-11-04 10:20:03 +0100

    InMemoryUserDetailsManager added!

--- src/main/java/com/raiseup/springMVC/security/SecurityConfig.java
+++ src/main/java/com/raiseup/springMVC/security/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +17,31 @@ public class SecurityConfig {
 
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user1= User.withUsername("user1")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails user2=User.withUsername("user1")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails user3= User.withUsername("user3")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin= User.withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2,user3);
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .csrf().disable();
-        return http.build();
+    return null;
     }
 }
